6bf8269560981caee8482a35b4b7a501fd6b1945
adib hasan <hasan07023@gmail.com>
2022-07-19 14:58:40 +0600

    uses of string

---
+++ useOfString.py
@@ -0,0 +1,14 @@
+first_name="Asif"
+last_name="Hasan"
+full_name=first_name+last_name
+print(full_name)
+full_name=first_name+" "+last_name
+print(full_name)
+first_name="Asif"
+last_name=" Hasan"
+full_name=first_name+last_name
+print(full_name)
+#print(full_name+3) error
+print(full_name+str(3))
+full_name = full_name+" "
+print(full_name*3)
