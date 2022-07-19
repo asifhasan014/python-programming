0733dd642afee9c51832ed8fa3a78d8eec22969d
adib hasan <hasan07023@gmail.com>
2022-07-19 15:40:34 +0600

    multiple variable declearation in one line

--- variables.py
+++ variables.py
@@ -10,3 +10,8 @@ var5=['a','b','c']
 print(var5)
 var6=['abc','bcd','cde']
 print(var6)
+
+name,age = "Asif Hasan Talukder", 30
+print("your name is "+name+" and your age is "+str(age))
+a=b=c=1
+print(a+b+c)
