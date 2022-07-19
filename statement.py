bbc6a45bc0ba8d2f8b35f0536880357ac4b6b8e7
adib hasan <hasan07023@gmail.com>
2022-07-20 01:21:50 +0600

    use of in operator

--- statement.py
+++ statement.py
@@ -1,7 +1,19 @@
-age = int(input("Enter your age : "))
-if age >= 1 and age < 14:
-    pass
-elif age >= 14 and age <= 35:
-    print("You are above 14 and you can play")
+# age = int(input("Enter your age : "))
+# if age >= 1 and age < 14:
+#     pass
+# elif age >= 14 and age <= 35:
+#     print("You are above 14 and you can play")
+# else:
+#     print("srry you can not play")
+
+# name="Asif Hasan"
+# if 'a' in name:
+#     print("a is present")
+# else:
+#     print("not present")
+
+"Asif Hasan"
+if 'a' in "Asif Hasan":
+    print("a is present")
 else:
-    print("srry you can not play")
+    print("not present")
