584e70d0bb25bf491df831e81e4a2c2f21ccba34
adib hasan <hasan07023@gmail.com>
2022-07-20 01:16:22 +0600

    if elif else condition with and or operator

---
+++ statement.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age : "))
+if age >= 1 and age < 14:
+    pass
+elif age >= 14 and age <= 35:
+    print("You are above 14 and you can play")
+else:
+    print("srry you can not play")
