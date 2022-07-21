9d4ab729f361cb4d9f37f3d96d4d94d7b39411c4
adib hasan <hasan07023@gmail.com>
2022-07-22 03:27:19 +0600

    use of dictionary

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+#First method to create a dictionay
+user = {'name':'Asif Hasan','age':30}
+print(user)
+#Second method to create a dictionay
+user2= dict(name='Asif Hasan Talukder',age=30)
+print(user2)
+#Access the value of a dictionary
+print(user2['name'])
+print(user2['age'])
+#How to add data in a empty dictionay
+user3={}
+user3['name']='Adib Hasan'
+user3['age']='32'
+print(user3)
+
+
+
