62cd037a48c84622eb2920277f885a7ec5641967
adib hasan <hasan07023@gmail.com>
2022-07-21 14:49:56 +0600

    function inside another function

--- function.py
+++ function.py
@@ -1,10 +1,22 @@
-def add_two_number(num1, num2):
-    return num1+num2
+# def add_two_number(num1, num2):
+#     return num1+num2
 
-a= int(input("input your first number: "))
-b= int(input("input your first number: "))
+# a= int(input("input your first number: "))
+# b= int(input("input your first number: "))
 
-total=add_two_number(a,b)
+# total=add_two_number(a,b)
 
-print("sum of two number is: "+ str(total))
-print(f"sum of two number is: {total}")
+# print("sum of two number is: "+ str(total))
+# print(f"sum of two number is: {total}")
+
+#Function Inside function
+
+def greater(a,b):
+    if a>b:
+        return a
+    return b
+
+def greatest(a,b,c):
+    return greater(greater(a,b),c)
+
+print(greatest(100,150,95))
