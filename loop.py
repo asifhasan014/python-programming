c3da61faaffff74d694f53684c5dcffcdf681e90
adib hasan <hasan07023@gmail.com>
2022-07-21 12:18:30 +0600

    number guess game using while loop

--- loop.py
+++ loop.py
@@ -1,7 +1,29 @@
-i=0
+# i=0
+# sum=0
 # while i<=10:
 #     print(f"{i}.Hello World")
 #     i=i+1
 
-for i in range(11):
-    print(f"Hello world: {i}")
+# for i in range(11):
+#     sum = sum + i
+# print(f"Hello sum: {sum}")
+
+winning_number = 43
+guess =1 
+number = int(input("guess a number between 1 to 100 : "))
+game_over = False
+
+while not game_over:
+    if number == winning_number:
+        print(f"you win the game and your attempt is {guess}")
+        game_over = True
+    else:
+        if number > winning_number:
+            print("Too High...")
+            guess += 1
+            number=int(input("guess again : "))
+        else:
+            print("Too Low...")
+            guess += 1
+            number=int(input("guess again : "))
+
