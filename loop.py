edb9d13daeb344ca6a15ae2dd160b76c8b708a81
adib hasan <hasan07023@gmail.com>
2022-07-21 12:44:43 +0600

    step argument with positive number

--- loop.py
+++ loop.py
@@ -4,27 +4,27 @@
 #     print(f"{i}.Hello World")
 #     i=i+1
 
-# for i in range(11):
-#     sum = sum + i
-# print(f"Hello sum: {sum}")
+for i in range(0,11,2):
+    # sum = sum + i
+    print(f"Hello sum: {i}")
 
 #DRY Principle = Do not repeat yourself(code)
-import random
-winning_number = random.randint(1,100)
-guess =1 
-number = int(input("guess a number between 1 to 100 : "))
-game_over = False
+#import random
+# winning_number = random.randint(1,100)
+# guess =1 
+# number = int(input("guess a number between 1 to 100 : "))
+# game_over = False
 
-while not game_over:
-    if number == winning_number:
-        print(f"you win the game and your attempt is {guess}")
-        game_over = True
-    else:
-        if number > winning_number:
-            print("Too High...")
-        else:
-            print("Too Low...")
+# while not game_over:
+#     if number == winning_number:
+#         print(f"you win the game and your attempt is {guess}")
+#         game_over = True
+#     else:
+#         if number > winning_number:
+#             print("Too High...")
+#         else:
+#             print("Too Low...")
 
-        guess += 1
-        number=int(input("guess again : "))
+#         guess += 1
+#         number=int(input("guess again : "))
 
