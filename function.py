c045f9aa107fdf609f09f11766f11a49554ed8c4
adib hasan <hasan07023@gmail.com>
2022-07-21 14:40:33 +0600

    define function

---
+++ function.py
@@ -0,0 +1,10 @@
+def add_two_number(num1, num2):
+    return num1+num2
+
+a= int(input("input your first number: "))
+b= int(input("input your first number: "))
+
+total=add_two_number(a,b)
+
+print("sum of two number is: "+ str(total))
+print(f"sum of two number is: {total}")
