15b0a32879a08b5f45f61f6c8d352b55fc9c27ca
adib hasan <hasan07023@gmail.com>
2022-07-22 11:31:15 +0600

    word matching problm with dictonary

--- dictionaries.py
+++ dictionaries.py
@@ -12,6 +12,21 @@ user3={}
 user3['name']='Adib Hasan'
 user3['age']='32'
 print(user3)
+#Third method to create a dictionay
+user4= dict.fromkeys(['name','age'],'unknown')
+user4['name']='Latul Hasan'
+print("name of user4 is "+ user4['name'])
+print("name of user4 is "+ user4.get('name'))
+print(user4)
 
+#Word counter problem
+print("\n******************** word counter *************************")
+def wordCount(word):
+    dictonary = {}
+    for char in word:
+        dictonary[char] = word.count(char)
+    return dictonary
+
+print(wordCount('AsifHasan'))
 
 
