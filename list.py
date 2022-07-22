df3fb2514d81c68caad2c720464abbf0ebabe77a
adib hasan <hasan07023@gmail.com>
2022-07-22 16:51:00 +0600

    list comprehansion with if else

--- list.py
+++ list.py
@@ -19,3 +19,5 @@ print(square)
 square2 = [i**2 for i in range(1,11)]
 print(f"second list: {square2}")
 # print("second list "+ str(square2))
+new_list2 = [i*2 if (i%2 == 0) else -i for i in range(1,11)]
+print(f"new_list2: {new_list2}")
