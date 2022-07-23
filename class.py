79c2eade56c4b03319a6b5f8622c682d50a5ebaf
adib hasan <hasan07023@gmail.com>
2022-07-23 14:25:33 +0600

    create method inside class

--- class.py
+++ class.py
@@ -4,7 +4,11 @@ class Person:
         self.last_name = last_name
         self.age = age
 
-p1 = Person("Asif","Hasan","24")
-p2 = Person("Adib","Hasan","34")
-print(p1.first_name+" "+ p1.last_name+" "+p1.age)
-print(p2.first_name+" "+ p2.last_name+" "+p2.age)
+    def isAbove_18(self):
+        return self.age>18
+
+p1 = Person("Asif","Hasan",24)
+p2 = Person("Adib","Hasan",34)
+print(p1.isAbove_18())
+print(p1.first_name+" "+ p1.last_name+" "+str(p1.age)+" age is above 18? "+ str(p1.isAbove_18()))
+print(p2.first_name+" "+ p2.last_name+" "+str(p2.age)+" age is above 18? "+ str(p2.isAbove_18()))
