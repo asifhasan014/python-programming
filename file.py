0f5cc9f1d9db4f73bf86b884545483687a9800dd
adib hasan <hasan07023@gmail.com>
2022-07-24 18:12:25 +0600

    data read and write at a time

--- file.py
+++ file.py
@@ -6,14 +6,19 @@
 # f.close()
 
 #file read with bock
-with open('file1.txt') as f:
-    data = f.read()
-    print(data)
+# with open('file1.txt') as f:
+#     data = f.read()
+#     print(data)
 
-with open('file2.txt','w') as f:
-    f.write("Hello Asif")
-print("Data is written")
+# with open('file2.txt','w') as f:
+#     f.write("Hello Asif")
+# print("Data is written")
 
-with open('file2.txt','a') as f:
-    f.write("\nHello Adib")
-print("Data append")
+# with open('file2.txt','a') as f:
+#     f.write("\nHello Adib")
+# print("Data append")
+
+#Read and write at a time
+with open('file1.txt','r') as rf:
+    with open('file3.txt','a') as wf:
+        wf.write(rf.read())
