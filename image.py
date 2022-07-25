97acd2bbc3b229c9a2b9b5526bd271df10b7f581
adib hasan <hasan07023@gmail.com>
2022-07-25 13:59:28 +0600

    loop on images

--- image.py
+++ image.py
@@ -1,14 +1,27 @@
 from PIL import Image
+import os
 
-img = Image.open('D:\cat-picture\img1.jpg')
-img.save('images/img1.jpg')
-img.save('images/img1.png')
-img.save('images/img1.pdf')
-img.show()
+# img = Image.open('D:\cat-picture\img1.jpg')
+# img.save('images/img1.jpg')
+# img.save('images/img1.png')
+# img.save('images/img1.pdf')
+# img.show()
 
 #thumbnail image
-MAX_SIZE = (150,150)
-img5 = Image.open('D:\cat-picture\img5.jpg')
-img5.thumbnail(MAX_SIZE)
-img5.save('images/catprofile.jpg')
+# MAX_SIZE = (150,150)
+# img5 = Image.open('D:\cat-picture\img5.jpg')
+# img5.thumbnail(MAX_SIZE)
+# img5.save('images/catprofile.jpg')
+# print(os.listdir('D:\cat-picture'))
+folder = 'D:\cat-picture'
+saveFolder = 'images'
+
+for item in os.listdir(folder):
+    print(folder+"\\"+item)
+    img  = Image.open(folder+"\\"+item)
+    filename,extension = os.path.splitext(item)
+    print(f"filename {filename} , extension {extension}")
+    img.save(saveFolder+"\\"+filename+'.png')
+
+
 
