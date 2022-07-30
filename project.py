131edabdb02412e969bc7e90e96e75152148aeb8
adib hasan <hasan07023@gmail.com>
2022-07-30 06:01:29 +0600

    how to make a function smaller and check weather a folder exist or not

--- project.py
+++ project.py
@@ -12,12 +12,13 @@ folderpath = input("Enter your folder path: ")
 
 
 def file_finder(folder_path, file_extensions):
-    files = []
-    for file in os.listdir(folder_path):
-        for extension in file_extensions:
-            if file.endswith(extension):
-                files.append(file)
-    return files
+    # files = []
+    # for file in os.listdir(folder_path):
+    #     for extension in file_extensions:
+    #         if file.endswith(extension):
+    #             files.append(file)
+    # return files
+    return [file for file in os.listdir(folder_path) for extension in file_extensions if file.endswith(extension)]
 
 
 # filesExtension = file_finder(folderpath, video_extension)
@@ -27,11 +28,15 @@ for extension_type, extension_touple in dict_extension.items():
     # print(file_finder(folderpath, extension_touple))
     folder_name = extension_type.split('_')[0]+'Files'
     folder_path = os.path.join(folderpath, folder_name)
-    print("new path to store the file : "+folder_path)
-    os.mkdir(folder_path)
-    for item in (file_finder(folderpath, extension_touple)):
-        item_path = os.path.join(folderpath, item)
-        print("item_path : "+item_path)
-        item_new_path = os.path.join(folder_path, item)
-        print("item_new_path : "+item_new_path)
-        shutil.move(item_path, item_new_path)
+    # print("new path to store the file : "+folder_path)
+    isdir = os.path.isdir(folder_path)
+    if not isdir:
+        os.mkdir(folder_path)
+        for item in (file_finder(folderpath, extension_touple)):
+            item_path = os.path.join(folderpath, item)
+            # print("item_path : "+item_path)
+            item_new_path = os.path.join(folder_path, item)
+            # print("item_new_path : "+item_new_path)
+            shutil.move(item_path, item_new_path)
+    else:
+        print("Folder already exists")
